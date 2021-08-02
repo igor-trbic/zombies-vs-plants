0354da17c6adee639be898c1b9e0b39e40e8a8bf
Igor Trbic <trbic.igor@gmail.com>
2021-08-02 23:35:02 +0200

    Initial setup; added simple animations

---
+++ ZombiesVsPlants/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    [SerializeField] int timeToWait = 4;
+    int currSceneIdx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currSceneIdx = SceneManager.GetActiveScene().buildIndex;
+        if (currSceneIdx == 0) {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime() {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(currSceneIdx+1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
