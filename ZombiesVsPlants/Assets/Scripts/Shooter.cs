a8d3cff9a266cd61e411d059ed144132e7436285
Igor Trbic <trbic.igor@gmail.com>
2021-08-06 20:17:26 +0200

    Added more defenders

--- ZombiesVsPlants/Assets/Scripts/Shooter.cs
+++ ZombiesVsPlants/Assets/Scripts/Shooter.cs
@@ -7,11 +7,47 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
 
+    AttackarSpawner myLaneSpawner;
+    Animator animator;
+
     public void Fire() {
         Instantiate(
             projectile,
             gun.transform.position,
             transform.rotation
         );
+    }
+
+    public void Start() {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update() {
+        if(isAttackerInLane()) {
+            animator.SetBool("isAttacking", true);
+        } else {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner() {
+        AttackarSpawner[] spawners = FindObjectsOfType<AttackarSpawner>();
+
+        foreach (AttackarSpawner spawner in spawners) {
+            bool isCloseEnough = (
+                Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon
+            );
+            if (isCloseEnough) {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool isAttackerInLane() {
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        }
+        return true;
     }
 }
